96c1548744f98f9f62837d882a60e8054bfc81e8
Matt Migliore <mmigliore311@gmail.com>
2019-02-04 13:04:07 -0500

    remove recipe serializer

--- app/serializers/recipe_serializer.rb
+++
@@ -1,4 +0,0 @@
-class RecipeSerializer < ActiveModel::Serializer
-  attributes :id, :name, :category, :area, :instructions, :image, :youtube, :tags, :source
-  has_many :ingredients, through: :recipe_ingredients
-end
