b6b392ecc3e7f392dce4bea1d2f4a6e7f3f18e93
Matt Migliore <mmigliore311@gmail.com>
2019-02-04 09:42:05 -0500

    add Recipe with_ingredients class method

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -3,4 +3,15 @@ class Recipe < ApplicationRecord
   has_many :users, through: :user_recipes
   has_many :recipe_ingredients
   has_many :ingredients, through: :recipe_ingredients
+
+  def self.with_ingredients(selections)
+    recipes = Recipe.all
+
+    recipes.select do |recipe|
+      recipe.ingredients
+      recipe_ingred_ids = recipe.ingredients.map {|ingred| ingred.id}
+      selections.all? {|ingred| recipe_ingred_ids.include?(ingred)}
+    end
+
+  end
 end
