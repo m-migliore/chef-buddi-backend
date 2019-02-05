361d0d16cb8fc903ec20babd96b666dc3998e2f3
Matt Migliore <mmigliore311@gmail.com>
2019-02-05 16:27:31 -0500

    add destory to user_recipe route

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
       resources :users, only: [:index, :show, :update]
       resources :recipes, only: [:index, :show, :update]
       resources :ingredients, only: [:index, :show, :update]
-      resources :user_recipes, only: [:index, :show, :create, :update]
+      resources :user_recipes, only: [:index, :show, :create, :destroy]
       resources :user_ingredients, only: [:index, :create, :destroy]
       resources :recipe_ingredients, only: [:index, :show, :update]
       post 'recipes/find', to: 'recipes#find'
