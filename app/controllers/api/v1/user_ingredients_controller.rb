4d9b9c2ca9b101d9c2ed31d507b335c8875108e9
Matt Migliore <mmigliore311@gmail.com>
2019-02-05 14:49:04 -0500

    add working manage ingred

--- app/controllers/api/v1/user_ingredients_controller.rb
+++ app/controllers/api/v1/user_ingredients_controller.rb
@@ -1,23 +1,32 @@
 class Api::V1::UserIngredientsController < ApplicationController
-  before_action :find_user_ingredient, only: [:show, :update, :destroy]
+  before_action :find_user_ingredient, only: [:destroy]
 
   def index
     @user_ingredients = UserIngredient.all
     render json: @user_ingredients
   end
 
-  def show
-    render json: @user_ingredient
-  end
-
-  def update
-    @user_ingredient.update(user_ingredient_params)
+  def create
+    @user_ingredient = UserIngredient.create(user_ingredient_params)
     if @user_ingredient.save
       render json: @user_ingredient, status: :accepted
     else
       render json: { errors: @user_ingredient.errors.full_messages }, status: :unprocessible_entity
     end
   end
+
+  # def show
+  #   render json: @user_ingredient
+  # end
+  #
+  # def update
+  #   @user_ingredient.update(user_ingredient_params)
+  #   if @user_ingredient.save
+  #     render json: @user_ingredient, status: :accepted
+  #   else
+  #     render json: { errors: @user_ingredient.errors.full_messages }, status: :unprocessible_entity
+  #   end
+  # end
 
   def destroy
     @user_ingredient.destroy
