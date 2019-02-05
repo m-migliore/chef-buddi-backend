e83a4a3ca33985bb4379bf90c30d6fc3cdbfeed1
Matt Migliore <mmigliore311@gmail.com>
2019-02-05 16:27:00 -0500

    add destory action to user_recipe controller

--- app/controllers/api/v1/user_recipes_controller.rb
+++ app/controllers/api/v1/user_recipes_controller.rb
@@ -1,5 +1,5 @@
 class Api::V1::UserRecipesController < ApplicationController
-  before_action :find_user_recipe, only: [:show, :update]
+  before_action :find_user_recipe, only: [:show, :destroy]
 
   def index
     @user_recipes = UserRecipe.all
@@ -19,13 +19,17 @@ class Api::V1::UserRecipesController < ApplicationController
     end
   end
 
-  def update
-    @user_recipe.update(user_recipe_params)
-    if @user_recipe.save
-      render json: @user_recipe, status: :accepted
-    else
-      render json: { errors: @user_recipe.errors.full_messages }, status: :unprocessible_entity
-    end
+  # def update
+  #   @user_recipe.update(user_recipe_params)
+  #   if @user_recipe.save
+  #     render json: @user_recipe, status: :accepted
+  #   else
+  #     render json: { errors: @user_recipe.errors.full_messages }, status: :unprocessible_entity
+  #   end
+  # end
+
+  def destroy
+    @user_recipe.destroy
   end
 
   private
