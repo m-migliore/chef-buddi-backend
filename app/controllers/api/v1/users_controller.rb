a56e12290db15e673fef4448163eccf1e1372a21
Matt Migliore <mmigliore311@gmail.com>
2019-02-07 12:44:31 -0500

    add create method to users controller

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -10,6 +10,15 @@ class Api::V1::UsersController < ApplicationController
     render json: @user
   end
 
+  def create
+    @user = User.create(user_params)
+    if @user.save
+      render json: @user, status: :accepted
+    else
+      render json: { errors: @user.errors.full_messages }, status: :unprocessible_entity
+    end
+  end
+
   def update
     @user.update(user_params)
     if @user.save
