c6b98d26c6eddae66d32b4cc306c5118a1832dc4
Matt Migliore <mmigliore311@gmail.com>
2019-02-11 18:13:02 -0500

    add serializer touch ups for user and mealplan

--- app/serializers/mealplan_serializer.rb
+++ app/serializers/mealplan_serializer.rb
@@ -1,4 +1,8 @@
 class MealplanSerializer < ActiveModel::Serializer
-  attributes :id, :user_id, :title, :created_at
+  attributes :id, :user_id, :title, :created_at, :mealCount
   has_many :recipes, through: :meals
+
+  def mealCount
+    object.meals.length
+  end
 end
--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,7 +1,6 @@
 class UserSerializer < ActiveModel::Serializer
   attributes :id, :username
-  # has_many :recipes, through: :user_recipes
   has_many :user_recipes, key: :recipes
-
   has_many :user_ingredients, key: :ingredients
+  has_many :mealplans
 end
