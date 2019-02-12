a678552ef63edc9ca11813f62069e341472547e9
Matt Migliore <mmigliore311@gmail.com>
2019-02-12 13:50:47 -0500

    add working delete on mealplan

--- app/controllers/api/v1/mealplans_controller.rb
+++ app/controllers/api/v1/mealplans_controller.rb
@@ -32,7 +32,10 @@ class Api::V1::MealplansController < ApplicationController
   # end
 
   def destroy
+    meals = @mealplan.meals.map{|meal| meal.id}
+    Meal.destroy(meals)
     @mealplan.destroy
+    render json: {status: "meal deleted"}
   end
 
   private
