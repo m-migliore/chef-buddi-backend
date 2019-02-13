f8e2e4ce8f7b74292ba7ed3b43fb97a216455e99
Matt Migliore <mmigliore311@gmail.com>
2019-02-13 09:26:25 -0500

    add shopping list to mealplan

--- app/models/meal.rb
+++ app/models/meal.rb
@@ -1,3 +1,5 @@
 class Meal < ApplicationRecord
   belongs_to :recipe
+  has_many :recipe_ingredients, through: :recipe
+  has_many :ingredients, through: :recipe_ingredients
 end
--- app/serializers/mealplan_serializer.rb
+++ app/serializers/mealplan_serializer.rb
@@ -1,5 +1,5 @@
 class MealplanSerializer < ActiveModel::Serializer
-  attributes :id, :user_id, :title, :created_at, :mealCount, :recipes
+  attributes :id, :user_id, :title, :created_at, :mealCount, :recipes, :shoppingList
   # has_many :recipes, through: :meals
 
   def mealCount
@@ -15,5 +15,29 @@ class MealplanSerializer < ActiveModel::Serializer
        image: meal.recipe.image,
      }
    end
+  end
+
+  def shoppingList
+    all_ingredients = []
+    # object.meals.ingredients.each do |ingredient|
+    #   all_ingredients << ingredient
+    # end
+
+    object.meals.each do |meal|
+      meal.ingredients.each do |ingredient|
+        all_ingredients << ingredient
+      end
+    end
+
+    cleaned_total_ingredients = all_ingredients.uniq
+
+    user = User.find(object.user_id)
+    user_ingredients = user.ingredients
+
+    shopping_list = cleaned_total_ingredients.select do |ingredient|
+      !user_ingredients.include?(ingredient)
+    end
+
+    shopping_list
   end
 end
